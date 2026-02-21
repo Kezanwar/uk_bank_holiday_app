62443de67ad21999ffbf36ae6d970ac6fae5f9cf
Kez Anwar <kezanwar@gmail.com>
2026-02-21 11:47:06 +0000

    [lib/axios] added axios->ProvlemDetail interceptor tests

---
+++ lib/__tests__/axios.test.ts
@@ -0,0 +1,53 @@
+import axios from "@/lib/axios";
+import MockAdapter from "axios-mock-adapter";
+import ProblemDetail from "../problem-detail";
+
+const mock = new MockAdapter(axios);
+
+afterEach(() => mock.reset());
+
+describe("axios interceptor", () => {
+  it("rejects with ProblemDetail on server error", async () => {
+    mock.onGet("/test").reply(404);
+    expect.assertions(3);
+    try {
+      await axios.get("/test");
+      fail("Should reject with Problem Detail");
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(ProblemDetail);
+      expect(error.status).toBe(404);
+      expect(error.type).toBe(
+        "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/" +
+          error.status,
+      );
+    }
+  });
+
+  it("rejects with ProblemDetail on network error", async () => {
+    mock.onGet("/test").networkError();
+    expect.assertions(3);
+    try {
+      await axios.get("/test");
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(ProblemDetail);
+      expect(error.status).toBe(0);
+      expect(error.type).toBe("about:blank");
+      /*somethings going on with mock adapter here in this case we don't have 
+     request or response so its being treated as config error*/
+    }
+  });
+
+  it("rejects with ProblemDetail on timeout", async () => {
+    mock.onGet("/test").timeout();
+    expect.assertions(3);
+    try {
+      await axios.get("/test");
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(ProblemDetail);
+      expect(error.status).toBe(0);
+      expect(error.type).toBe("about:blank");
+      /*somethings going on with mock adapter here in this case we don't have 
+     request or response so its being treated as config error*/
+    }
+  });
+});
