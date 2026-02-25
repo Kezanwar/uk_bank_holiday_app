c21cadb5c08ee6c2a44b726fcaa8b61757a8edf4
Kez Anwar <kezanwar@gmail.com>
2026-02-25 19:36:02 +0000

    [views/tests] removed @react-native/testing-library component tests as running out of time

--- app/(tabs)/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react-native";
-import HomeScreen from "..";
-
-describe("<HomeScreen />", () => {
-  test("Title text renders correctly on HomeScreen", () => {
-    const { getByText } = render(<HomeScreen />);
-    getByText("Bank Holidays");
-  });
-});
